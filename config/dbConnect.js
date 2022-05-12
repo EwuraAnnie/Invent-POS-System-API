bc0f58b303851547781d6d4264ba225ede391ccd
EwuraAnnie <ewuranhyira03@gmail.com>
2022-05-12 11:34:11 +0000

    updated database connection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/invent")
+  .connect(
+    "mongodb+srv://Sinnie:Sinnie@cluster0.rnwsu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
   .then(() => console.log("Database connected successfully"));
