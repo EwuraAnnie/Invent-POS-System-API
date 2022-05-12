01e72aa16b22d8a9b9ba0364a06a0eb4936703cc
EwuraAnnie <ewuranhyira03@gmail.com>
2022-05-12 17:00:12 +0000

    updated database connection

--- upload/upload.js
+++ upload/upload.js
@@ -1,31 +1,52 @@
 const multer = require("multer");
-const path = require("path");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-let storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "upload");
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}-${Math.random() * 1000}${path.extname(
-        file.originalname
-      )}`
-    );
-  },
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(new Error("File formate should be PNG, JPG, JPEG"), false);
-  }
-};
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "demo",
+  allowedFormats: ["jpg", "png", "jpeg"],
+  transformation: [{ width: 500, height: 500, crop: "limit" }],
+});
 
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+const upload = multer({ storage: storage });
+
 module.exports = upload;
+
+// const multer = require("multer");
+// const path = require("path");
+
+// let storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "upload");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       `${file.fieldname}-${Date.now()}-${Math.random() * 1000}${path.extname(
+//         file.originalname
+//       )}`
+//     );
+//   },
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   if (
+//     file.mimetype === "image/png" ||
+//     file.mimetype === "image/jpg" ||
+//     file.mimetype === "image/jpeg"
+//   ) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("File formate should be PNG, JPG, JPEG"), false);
+//   }
+// };
+
+// const upload = multer({ storage: storage, fileFilter: fileFilter });
+// module.exports = upload;
